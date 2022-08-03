45e42754f2fa689e210653655a9b1b9be77cea98
Sabbir <webexpertsabbir@gmail.com>
2022-08-03 23:07:53 +0600

    parametar

--- parametar.js
+++ parametar.js
@@ -1,5 +1,24 @@
-function bringSingra(mony){
-    console.log('Mama singra den');
-    console.log(mony);
+// function bringSingra(mony){
+//     console.log('Mama singra den');
+//     console.log(mony);
+// }
+// bringSingra(250);
+
+// parametar special
+
+// function bringSingra(money){
+//     console.log('Taka paisen:', money)
+//     console.log('Mama singra den');
+// }
+// // bringSingra(300); 
+// // var taka = 300;
+// var taka = "dekhen";
+// bringSingra(taka);
+
+function num(num1, num2){
+    console.log(num1, num2);
+    var num = num1 + num2;
+    console.log(num);
+
 }
-bringSingra(250);
+num(40);
