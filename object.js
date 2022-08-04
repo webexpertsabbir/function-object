bd7ff7121c798fff23914d64db93761702488323
Sabbir <webexpertsabbir@gmail.com>
2022-08-04 14:08:57 +0600

    object

---
+++ object.js
@@ -0,0 +1,11 @@
+var mobile = {
+    brand: 'Sumsung', //value
+    price: 19000,
+    camera: '84px',
+    storage: '64gb'
+}
+
+console.log(mobile);
+mobile.storage = '12gb'
+console.log(mobile.camera);
+console.log(mobile)
